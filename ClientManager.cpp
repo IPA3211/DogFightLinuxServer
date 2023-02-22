592e65d9d8dcf646169397990fd194a0a5d639a9
ipa3211 <kygsm2003@naver.com>
2023-02-22 11:16:12 +0900

    Add clientManager.cpp

---
+++ ClientManager.cpp
@@ -0,0 +1,16 @@
+class ClientManager
+{
+private:
+    /* data */
+public:
+    ClientManager(/* args */);
+    ~ClientManager();
+};
+
+ClientManager::ClientManager(/* args */)
+{
+}
+
+ClientManager::~ClientManager()
+{
+}
